70529e16a93ca5d76346599213a218669516f5f7
Daniel Brain <dbrain@paypal.com>
2018-04-10 10:27:54 -0700

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e;
+
+if ! git diff-files --quiet; then
+    echo "Can not publish with unstaged uncommited changes";
+    exit 1;
+fi;
+
+if ! git diff-index --quiet --cached HEAD; then
+    echo "Can not publish with staged uncommited changes";
+    exit 1;
+fi;
+
+npm test;
+npm version ${1-patch};
+
+git push;
+git push --tags;
+npm publish;
